09c3c7393ace4f17a93ee2bc6b5e5101bda1b2f4
Serhii Ovchinnikov <50240729+ovchinnikovs@users.noreply.github.com>
2019-08-27 15:12:50 +0300

    Solution

---
+++ main.js
@@ -0,0 +1,28 @@
+
+$(function () {
+  $('#datepicker').datepicker({
+    format: "dd/mm/yyyy",
+    autoclose: true,
+    todayHighlight: true,
+    showOtherMonths: true,
+    selectOtherMonths: true,
+    autoclose: true,
+    changeMonth: true,
+    changeYear: true,
+    orientation: "button"
+  });
+});
+
+$(function () {
+  $('#datepicker2').datepicker({
+    format: "dd/mm/yyyy",
+    autoclose: true,
+    todayHighlight: true,
+    showOtherMonths: true,
+    selectOtherMonths: true,
+    autoclose: true,
+    changeMonth: true,
+    changeYear: true,
+    orientation: "button"
+  });
+});
